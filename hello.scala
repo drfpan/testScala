54fb1563952cfcdbdc633eadf318130f6e0d1c8d
drfpan <drfpan@sina.com>
2021-11-22 01:56:46 -0800

    first add

---
+++ hello.scala
@@ -0,0 +1,13 @@
+object hello {
+  def main(args: Array[String]): Unit = {
+    val k = 1 to (10,2)
+//    def functionName ([参数列表]) : [return type] = {
+//      function body
+    //def fn1(i:Int) {println(i)}
+    k.foreach(i=>println(i));
+}
+    //    for (i <- k) {
+//      println("i=" + i )
+//    }
+
+}
